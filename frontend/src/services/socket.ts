99dfe3cb7c9705aa1f7b480b9d6794357f52c99d
YashXBansal <yashbansal414@gmail.com>
2025-09-16 20:02:48 +0530

    making changes in the frontend for url change

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -1,6 +1,5 @@
 import { io } from "socket.io-client";
 
-const VITE_BACKEND_URL =
-  import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+export const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const socket = io(VITE_BACKEND_URL);
